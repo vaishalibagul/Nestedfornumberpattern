9aa3b80c10d73c7f81cab2b73c8bd6d6ac9a2cfc
vaishali bagul <106953326+vaishalibagul@users.noreply.github.com>
2022-07-01 13:14:19 +0530

    Add files via upload

---
+++ nestedforpatternk12.java
@@ -0,0 +1,43 @@
+//pattern12
+//flyods triangle
+
+/*
+
+1
+2 7
+3 8 13
+4 9 14 19
+5 10 15 20 25
+*/
+
+
+
+
+
+
+
+class  nestedforpatternk12
+{
+public static void main(String[]args)
+{
+int i,j,k;
+
+                    
+	for(i=1;i<=5;i++)
+	   {
+                         k=i;
+	       for(j=i;j>=1;j--)
+	           {
+                                    
+	             System.out.print(k+" ");
+                             k=k+5;
+                        
+                                 }
+System.out.println();
+                            
+                         }
+}
+}
+                    
+
+                    
